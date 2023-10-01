f1ec0b2b187e4d510694438e404b31921eabc326
anmolghai02 <theanmolghai@gmail.com>
2023-10-01 14:35:09 +0530

    updated notes with more info about data type

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,32 @@
+//COMPARISIONS OF DIFFERNET DATATYPES
+//here conversion happens
+console.log("2">1); //o/p true
+//here the js has converted to STRING to NUMBER
+console.log("2a">1);
+//here 2a was converted to the NaN which  
+
+//for this unliked data comparison problem & other functions, People started to use extended version of js called TypeScript.
+
+console.log(null > 0); //false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+//Reason : equality check == and comparision >,<,>=,<= works differently .
+//comparisions convert null to number ,treating it as 0.
+//in equality it is different.
+//so when null is 0 in comparisons.
+
+//Remember the comparisions and equality works different.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+//here all are false.
+
+
+//strict check : === , it checks with datatype .
+//there is no conversion .
+
+//try to avoid comparisions of  null and undefined :: so that the predicitve output comes.
+
+
+
