0c532e5f559ebd4e357a89fb392b2b853ebf93dc
anmolghai02 <theanmolghai@gmail.com>
2023-11-03 19:09:57 +0530

    added 03_arrow.js

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,65 @@
+//this keyword
+
+const user = {
+    username : "hitesh",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} , welcome to website`)   //without this keyword -> error
+        
+        console.log(this)  //o/p : this object we wrote will be output
+    }
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()  //username changed one
+
+console.log(this)  //o/p : {} empty object
+//REMEBER
+//but in console :: it will Window object (it is global object in browsers)
+
+//ARROW FUNCTION
+function chai1(){
+    let username = "hitesh"
+    console.log(this.username) //o/p : undefined it only work for objects
+    console.log(this);       //this in function
+    // here many things will come up
+}
+chai1()
+
+const chai2 = function(){
+    let username = "hitesh"
+    console.log(this.username) //o/p : still undefined
+}
+chai2()
+
+    //arrow fun
+const chai3 = () => {
+    let username = "hitesh"
+    console.log(this.username) //o/p : still undefined
+    console.log(this);   //o/p : {} empty object
+}
+chai3();
+
+() => {}       //this is basic arrow funciton
+//you can add it under a variable
+const addtwo = (num1 , num2) => {
+    return num1+num2;
+}
+console.log(addtwo(3,4))
+
+//implicit return                 -> highly used in the REACT
+//means you don't need to write return -> it wil automatically return
+const addTwo = (num1 , num2) => num1+num2  //you can wrap in parenthesis. but if braces used you have to use the return keyword   
+//it will return automatically.
+console.log(addTwo(3,4)) //op : 7
+
+//BUT TO RETURN YOU HAVE TO USE THE PARANTHESIS
+const add = () => ({username : "hitesh"})   //object va
+console.log(add()) 
+
+//syntax : for future
+const myArray = [2,5,3,7,8];
+myArray.forEach(() => {})   //this for each loop
+myArray.forEach(function(){}) 
