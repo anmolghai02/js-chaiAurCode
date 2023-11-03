5c4d41a50c164a36cb4d1045a06883f3ff654fd8
anmolghai02 <theanmolghai@gmail.com>
2023-11-03 19:25:21 +0530

    adding 04_IIFE concept

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,22 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+//with Iffe functions you have to use the ; .
+//to end the code run
+
+//named IFFE
+(function chai(){
+    console.log(`DB connected`)
+})();    //second () nal execution hogye
+//first () will make the function IIFE means imediatly invoke function
+
+//this is normal/unnamed IFFE
+( (name) => {
+    console.log(`DB connected 2 ${name}`)
+})('hitesh');     //with argument also.
+
+//parameterized IFFE
+
+
+
+
+
