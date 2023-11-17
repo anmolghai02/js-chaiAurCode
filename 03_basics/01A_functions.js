10aadb8a16fe2cc5ca7c1c1f511c42c675dd3f8f
anmolghai02 <theanmolghai@gmail.com>
2023-11-17 17:25:49 +0530

    updating 01A code file

--- 03_basics/01A_functions.js
+++ 03_basics/01A_functions.js
@@ -1,14 +1,14 @@
-//rest operator : same symbol as spread operator  but here it will be called rest operator
+//REST OPERATOR : same symbol as spread operator  but here it will be called rest operator
 function calculateCartPrice(...num1){
     return num1
 }
 //useCase : in cart we don't know amount there will be -> random amount is.  
-
 console.log(calculateCartPrice(200,400,500))
 //o/p : will be array [200,400,500]
 //if paramter was (var1 , ...num) then o/p : [400,500]
 
-//Function and object
+
+//Function and object parameter
 const user = {
     username : "hitesh",
     price : 199
@@ -26,7 +26,8 @@ handleObject({            //we can directly pass the object (like given)
 //there is no type checking in js (we have to use if-else) 
 //for this reason people use typescript
 
-//Function and array
+
+//Function and array parameter
 const myNewArray = [200,400,100,600]
 function returnSecondValue(getArray){
     return getArray[1]
