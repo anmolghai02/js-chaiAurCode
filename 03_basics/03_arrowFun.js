1341946a908f60e05763ecb406b4907d9a8785a5
anmolghai02 <theanmolghai@gmail.com>
2023-11-17 17:30:34 +0530

    updating 03 code file

--- 03_basics/03_arrowFun.js
+++ 03_basics/03_arrowFun.js
@@ -1,13 +1,11 @@
 //this keyword
-
 const user = {
     username : "hitesh",
     price : 999,
-
+    
     welcomeMessage : function(){
         console.log(`${this.username} , welcome to website`)   //without this keyword -> error
-        
-        console.log(this)  //o/p : this object we wrote will be output
+        console.log(this)  //o/p : the object we wrote will be output
     }
 }
 
@@ -19,28 +17,33 @@ console.log(this)  //o/p : {} empty object
 //REMEBER
 //but in console :: it will Window object (it is global object in browsers)
 
-//ARROW FUNCTION
+
+//this in function .
 function chai1(){
     let username = "hitesh"
     console.log(this.username) //o/p : undefined it only work for objects
-    console.log(this);       //this in function
-    // here many things will come up
+    console.log(this);       
+    //o/p: here many things will come up
 }
 chai1()
 
 const chai2 = function(){
     let username = "hitesh"
     console.log(this.username) //o/p : still undefined
+    console.log(this)
+    //o/p: here many things will come up
 }
 chai2()
 
-    //arrow fun
+//ARROW FUNCTION and This 
 const chai3 = () => {
     let username = "hitesh"
     console.log(this.username) //o/p : still undefined
-    console.log(this);   //o/p : {} empty object
+    console.log(this);   //o/p : {} means empty object
 }
 chai3();
+
+//Arrow Function
 
 () => {}       //this is basic arrow funciton
 //you can add it under a variable
@@ -55,11 +58,12 @@ const addTwo = (num1 , num2) => num1+num2  //you can wrap in parenthesis. but if
 //it will return automatically.
 console.log(addTwo(3,4)) //op : 7
 
-//BUT TO RETURN YOU HAVE TO USE THE PARANTHESIS
+//BUT TO RETURN Object YOU HAVE TO USE THE PARANTHESIS
 const add = () => ({username : "hitesh"})   //object va
 console.log(add()) 
 
 //syntax : for future
 const myArray = [2,5,3,7,8];
 myArray.forEach(() => {})   //this for each loop
+//or
 myArray.forEach(function(){}) 
